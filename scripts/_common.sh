9444a5fb49a4d7c4b317439337bae5c0b922a427
ericgaspar <junk.eg@free.fr>
2021-08-01 22:31:07 +0200

    Fix

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -4,6 +4,8 @@
 # COMMON VARIABLES
 #=================================================
 
+version_commit=062b92be29d99eb632c9821b5ac219fd4590ef8c
+
 # dependencies used by the app
 pkg_dependencies="postgresql apt-transport-https"
 
